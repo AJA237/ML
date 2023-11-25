9380a4c8e6252c3d834f5db28d9bf6e4217a95ff
AJA237 <azumbij@gmail.com>
2023-11-25 05:50:35 +0000

    Nov 25, 2023, 6:50 AM

---
+++ closest_landfill.py
@@ -0,0 +1,17 @@
+class ClosestLandFill:
+    def __init__(self, available_landfill, request):
+        self.landfills = available_landfill
+        self.request = request
+        self.lf = None
+        self.dist = 0
+        
+    def get_landfill_loc(self):
+        
+        return
+    def get_request_loc(self):
+        return
+    def distance(self):
+        return
+    def landfill(self):
+        return
+    
