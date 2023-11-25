637edb5aaeea67f6fd980bd147d14a478e337314
AJA237 <azumbij@gmail.com>
2023-11-25 06:08:38 +0000

    Nov 25, 2023, 7:08 AM

--- closest_landfill.py
+++ closest_landfill.py
@@ -3,7 +3,7 @@ class ClosestLandFill:
     def __init__(self, available_landfill, request):
         self.landfills = available_landfill
         self.request = request
-        self.lf = None
+        self.lfs = None
         self.dist = 0
 
     def get_landfill_loc(self):
@@ -16,12 +16,12 @@ class ClosestLandFill:
     Returns:
     - landfill_loc: A dictionary containing the location of each landfill object.
     """
-        landfill_loc = {landfill:landfill.location for landfill in self.landfills}
-        return landfill_loc
+        self.lfs = {landfill:landfill.location for landfill in self.landfills}
+        return self.lfs
     def get_request_loc(self):
         return self.request.location
     def distance(self):
-
+        
         return
     def landfill(self):
         return
