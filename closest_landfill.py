e6bfd61682903fb14a5d32f75e79ff9d1c63ba22
AJA237 <azumbij@gmail.com>
2023-11-25 06:20:00 +0000

    Nov 25, 2023, 7:20 AM

--- closest_landfill.py
+++ closest_landfill.py
@@ -1,4 +1,5 @@
-clear
+import math
+
 class ClosestLandFill:
     def __init__(self, available_landfill, request):
         self.landfills = available_landfill
@@ -21,8 +22,24 @@ class ClosestLandFill:
     def get_request_loc(self):
         return self.request.location
     def distance(self):
+        # distance between latitudes
+        # and longitudes
+        for landfill,loc in self.lfs:
+            dLat = (self.lfs[landfill][0] - self.request.location[0]) * math.pi / 180.0
+            dLon = (self.lfs[landfill][1] - self.request.location[1]) * math.pi / 180.0
         
-        return
+            # convert to radians
+            lat1 = (lat1) * math.pi / 180.0
+            lat2 = (lat2) * math.pi / 180.0
+        
+            # apply formulae
+            a = (pow(math.sin(dLat / 2), 2) +
+                pow(math.sin(dLon / 2), 2) *
+                    math.cos(lat1) * math.cos(lat2));
+            rad = 6371
+            c = 2 * math.asin(math.sqrt(a))
+            return rad * c
+    
     def landfill(self):
         return
     
