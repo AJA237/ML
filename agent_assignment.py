4a47083ac3207b00ccfda154616c6f87c8f160be
AJA237 <azumbij@gmail.com>
2023-11-25 08:17:31 +0000

    Nov 25, 2023, 9:17 AM

--- agent_assignment.py
+++ agent_assignment.py
@@ -2,5 +2,7 @@ class AgentAssign:
     def __init__(self, request, agents_list):
         self.request = request
         self.agents = agents_list
-    def closest_agent():
+    def closest_agent(self):
+        return
+    def distance(self):
         return
