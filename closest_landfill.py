31d94613f1b5817147265ac7d1e76bfc396ff586
AJA237 <azumbij@gmail.com>
2023-11-25 06:03:15 +0000

    Nov 25, 2023, 7:03 AM

--- closest_landfill.py
+++ closest_landfill.py
@@ -7,6 +7,15 @@ class ClosestLandFill:
         self.dist = 0
 
     def get_landfill_loc(self):
+        """
+    Retrieves the location of each landfill object and stores it in a dictionary.
+
+    Parameters:
+    - self: The current instance of the class.
+
+    Returns:
+    - landfill_loc: A dictionary containing the location of each landfill object.
+    """
         landfill_loc = {landfill:landfill.location for landfill in self.landfills}
         return
     def get_request_loc(self):
