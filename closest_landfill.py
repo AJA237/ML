67ff47ea3258de71061f6f6ea3726ba1e9391d50
AJA237 <azumbij@gmail.com>
2023-11-25 06:33:06 +0000

    Nov 25, 2023, 7:33 AM

--- closest_landfill.py
+++ closest_landfill.py
@@ -9,21 +9,26 @@ class ClosestLandFill:
 
     def get_landfill_loc(self):
         """
-    Retrieves the location of each landfill object and stores it in a dictionary.
+        Retrieves the location of each landfill object and stores it in a dictionary.
 
-    Parameters:
-    - self: The current instance of the class.
+        Parameters:
+        - self: The current instance of the class.
 
-    Returns:
-    - landfill_loc: A dictionary containing the location of each landfill object.
-    """
+        Returns:
+        - landfill_loc: A dictionary containing the location of each landfill object.
+        """
         self.lfs = {landfill:landfill.location for landfill in self.landfills}
         return self.lfs
     def get_request_loc(self):
         return self.request.location
     
     def distance(self):
-        
+        """
+        Calculates the distance between the current location and the closest landfill.
+
+        The code iterates through each landfill and calculates the distance using the haversine formula. It then updates the 'dist' variable with the minimum distance found.
+
+        """
         # distance between latitudes
         # and longitudes
         for landfill,loc in self.lfs:
@@ -43,7 +48,8 @@ class ClosestLandFill:
             distance = rad * c
             if distance < self.dist:
                 self.dist = distance
+                self.lfs = self.lfs[landfill]
     
     def landfill(self):
-        return
+        return self.lfs
     
