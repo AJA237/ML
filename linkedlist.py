c6f6ff2ef663060b4a138d4703beda027329e407
AJA237 <azumbij@gmail.com>
2023-12-03 02:35:56 +0000

    Dec 3, 2023, 3:35 AM

--- linkedlist.py
+++ linkedlist.py
@@ -7,3 +7,19 @@ class Node:
     def __str__(self):
         return self.value
 
+class DoubleLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+    
+    def append(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.previous = self.tail
+            self.tail = new_node
+        self.length += 1
